04f51ff240fb53eadc10337531249c21b7429643
j.sadi <j.sadi63@gmail.com>
2022-06-26 17:22:53 +0430

    first commit.

---
+++ ringBuffer.c
@@ -0,0 +1,95 @@
+/*
+ * ringBuffer.c
+ *
+ *  Created on: May 23, 2022
+ *      Author: Sadi
+ */
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "ringBuffer.h"
+
+
+uint8_t ringBuffer_init(ringBuffer_t *rbuff,uint16_t lenght)
+{
+	rbuff->buffer = (uint8_t*)malloc(sizeof(uint8_t*) * lenght);
+	rbuff->lenght = lenght;
+	rbuff->rIndex = 0;
+	rbuff->wIndex = 0;
+	if(rbuff->buffer != NULL)
+		return 1;
+	else
+		return 0;
+}
+
+void ringBuffer_push(ringBuffer_t *rbuff,uint8_t input)
+{
+	rbuff->buffer[rbuff->wIndex++] = input;
+	if(rbuff->wIndex >= rbuff->lenght)
+	{
+		rbuff->wIndex = 0;
+	}
+}
+
+uint8_t ringBuffer_get(ringBuffer_t *rbuff,uint16_t index)
+{
+	return rbuff->buffer[index];
+}
+
+uint8_t ringBuffer_get_offset(ringBuffer_t *rbuff,uint16_t index,int offset)
+{
+	uint8_t result = 0;
+	if ((index + offset) <= (rbuff->lenght-1))
+	{
+		result = rbuff->buffer[index + offset];
+	}
+	else if ((index + offset) > (rbuff->lenght-1))
+	{
+		result = rbuff->buffer[index + offset - rbuff->lenght];
+	}
+	else if ((index + offset) < 0)
+	{
+		result = rbuff->buffer[rbuff->lenght + (index + offset)];
+	}
+	return result;
+}
+
+void ringBuffer_add_rIndex(ringBuffer_t *rbuff,int count)
+{
+	if ((rbuff->rIndex + count) <= (rbuff->lenght-1))
+	{
+		rbuff->rIndex += count;
+	}
+	else if ((rbuff->rIndex + count) > (rbuff->lenght-1))
+	{
+		rbuff->rIndex = rbuff->rIndex + count - rbuff->lenght;
+	}
+	else if ((rbuff->rIndex + count) < 0)
+	{
+		rbuff->rIndex = rbuff->lenght + (rbuff->rIndex + count);
+	}
+}
+
+uint16_t ringBuffer_exiting_bytes(ringBuffer_t *rbuff)
+{
+	uint16_t count=0;
+	if (rbuff->wIndex > rbuff->rIndex)
+	{
+		count = rbuff->wIndex - rbuff->rIndex;
+	}
+	else if (rbuff->wIndex < rbuff->rIndex)
+	{
+		count = (rbuff->lenght - rbuff->rIndex) + rbuff->wIndex;
+	}
+	else if (rbuff->wIndex == rbuff->rIndex)
+	{
+		count = 0;
+	}
+
+	return count;
+}
+
+void ringBuffer_reset_wIndex(ringBuffer_t *rbuff)
+{
+	rbuff->wIndex = 0;
+}
---
+++ ringBuffer.h
@@ -0,0 +1,26 @@
+/*
+ * ringBuffer.h
+ *
+ *  Created on: May 23, 2022
+ *      Author: Sadi
+ */
+
+#ifndef INC_RING_BUFFER_H_
+#define INC_RING_BUFFER_H_
+
+typedef struct ringBuffer
+{
+	uint8_t *buffer;
+	uint16_t lenght;
+	uint16_t rIndex,wIndex;
+}ringBuffer_t;
+
+uint8_t ringBuffer_init(ringBuffer_t *rbuff,uint16_t lenght);
+void ringBuffer_push(ringBuffer_t *rbuff,uint8_t input);
+uint8_t ringBuffer_get(ringBuffer_t *rbuff,uint16_t index);
+uint8_t ringBuffer_get_offset(ringBuffer_t *rbuff,uint16_t index,int offset);
+void ringBuffer_add_rIndex(ringBuffer_t *rbuff,int count);
+uint16_t ringBuffer_exiting_bytes(ringBuffer_t *rbuff);
+void ringBuffer_reset_wIndex(ringBuffer_t *rbuff);
+
+#endif /* INC_RING_BUFFER_H_ */
